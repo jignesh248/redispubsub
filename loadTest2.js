99c0cbc4623e686be0f8f690c55d070e21c6dad5
Jignesh <jignesh2481991@gmail.com>
2019-04-17 12:05:56 +0530

    added all files

---
+++ loadTest2.js
@@ -0,0 +1,129 @@
+let argv = require('minimist')(process.argv.slice(2));
+let socketClusterClient = require('socketcluster-client');
+let cluster = require('cluster');
+let os = require('os');
+
+let testCooldownDelay = Number(argv.cooldown || 5000);
+
+let serverHostname = argv.hostname || 'localhost';
+let serverPort = Number(argv.port || 8000);
+let numClients = Number(argv.clients || 1000);
+// The test type.
+let test = argv.test || 'many-subscribers';
+
+let cpuCount = Number(argv.cpus || os.cpus().length);
+let numClientsPerCPU = Math.round(numClients / cpuCount);
+
+if (cluster.isMaster) {
+    console.log('Test client CPUs used:', cpuCount);
+    console.log('serverHostname:', serverHostname);
+    console.log('serverPort:', serverPort);
+    console.log('numClients:', numClients);
+
+    let workerList = [];
+    var workersReadyPromises = [];
+
+    for (let i = 0; i < cpuCount; i++) {
+        let worker = cluster.fork();
+        workerList.push(worker);
+        workersReadyPromises.push(
+            new Promise((resolve, reject) => {
+                worker.once('message', (packet) => {
+                    if (packet.type == 'success') {
+                        resolve();
+                    } else if (packet.type == 'error') {
+                        reject(new Error(packet.message));
+                    }
+                });
+            })
+        );
+    }
+
+    Promise.all(workersReadyPromises)
+        .then((results) => {
+            console.log('All clients are connected... Waiting for CPUs to cool down before starting the test.');
+            // Wait a bit before starting the test.
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve();
+                }, testCooldownDelay);
+            });
+        })
+        .then(() => {
+            /* var controlSocket = socketClusterClient.connect({
+                hostname: serverHostname,
+                port: serverPort,
+                multiplex: false
+            });
+
+            if (test === 'many-subscribers') {
+                console.log(`Starting the ${test} test.`);
+
+                setInterval(() => {
+                    console.log('Publishing a single message string to the testChannel');
+                    controlSocket.publish('testChannel', 'hello' + Math.round(Math.random() * 1000));
+                }, 1000);
+            } */
+        })
+        .catch((err) => {
+            console.log('Failed to setup some workers. ' + err.message);
+        });
+
+} 
+else {
+    let socketList = [];
+
+    for (let i = 0; i < numClientsPerCPU; i++) {
+        socketList.push(
+            socketClusterClient.connect({
+                hostname: serverHostname,
+                port: serverPort,
+                multiplex: false,
+                autoConnect: false
+            })
+        );
+    }
+    socketList.forEach((socket) => {
+        socket.connect();
+    });
+
+    if (test === 'many-subscribers') {
+
+        socketList.forEach((socket) => {
+            let testChannel = socket.subscribe('match1');
+
+            testChannel.watch((data) => console.log('received msg %s %s', data, Date.now()));
+
+        });
+
+        /*
+        let subscribePromises = [];
+
+        socketList.forEach((socket) => {
+          let testChannel = socket.subscribe('testChannel');
+          subscribePromises.push(
+            new Promise((resolve, reject) => {
+              testChannel.once('subscribe', () => {
+                testChannel.off('subscribeFail');
+                resolve();
+              });
+              testChannel.once('subscribeFail', (err) => {
+                testChannel.off('subscribe');
+                reject(err);
+              });
+            })
+          );
+        });
+        Promise.all(subscribePromises)
+        .then((results) => {
+          process.send({type: 'success'});
+        })
+        .catch((err) => {
+          process.send({type: 'error', message: err.message});
+        });
+      */
+    } 
+    else {
+        console.error(`No '${test}' test exists`);
+    }
+}
---
+++ worker.js
@@ -0,0 +1,82 @@
+var SCWorker = require('socketcluster/scworker');
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var morgan = require('morgan');
+var healthChecker = require('sc-framework-health-check');
+const url = require('url');
+const Redis = require('ioredis');
+const redis = new Redis();
+const connections = process.env.connections || 1000
+
+redis.subscribe('match1', 'match2', 'match3', 'match4', 'match5', function (err, count) {
+  if(err){
+    console.log(err)
+  }   
+});
+
+class Worker extends SCWorker {
+  run() {
+    console.log('   >> Worker PID:', process.pid);
+    var environment = this.options.environment;
+
+    var app = express();
+
+    var httpServer = this.httpServer;
+    var scServer = this.scServer;
+
+    if (environment === 'dev') {
+      // Log every HTTP request. See https://github.com/expressjs/morgan for other
+      // available formats.
+      app.use(morgan('dev'));
+    }
+    app.use(serveStatic(path.resolve(__dirname, 'public')));
+
+    // Add GET /health-check express route
+    healthChecker.attach(this, app);
+
+    httpServer.on('request', app);
+
+    var count = 0;
+
+    /*
+      In here we handle our incoming realtime connections and listen for events.
+    */
+    scServer.on('connection', function (socket) {
+
+      //console.log('new socket #' + socket.id + ' connected in (' + process.pid + ')');
+      count += 1
+      
+      if(count == connections){
+        console.log("all connected")
+      }
+
+      socket.on('disconnect', function (data) {
+        console.log("one client disconnected")
+      });
+
+			socket.on('message', function (event) {
+        //if(event && event["data"] && event["data"]["channel"] && event["data"]["channel"] == "ping"){
+        //  console.log('message (' + process.pid + "): "+event.data.data);
+        //}
+			});
+
+			socket.on('close', function (data) {
+				console.log('socket #' + socket.id + ' closed in (' + process.pid + "): ");
+			});
+
+			socket.on('error', function (data) {
+				console.log('error in (' + process.pid + "): ", data);
+      });
+      
+    });
+
+    redis.on('message', function (channel, message) {
+      console.log('Received message %s from channel %s on server', message, channel);
+      scServer.exchange.publish(channel, message);
+    });
+    
+  }
+}
+
+new Worker();
